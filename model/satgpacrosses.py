883ba5452764a95d298dd938dd27b1a56d16fda6
lakshusan <lakshanya.sankaran@gmail.com>
2024-04-19 19:03:26 -0700

    remove additional comment

--- model/satgpacrosses.py
+++ model/satgpacrosses.py
@@ -9,33 +9,11 @@ class SATtoGPAModel:
 
     def predict(self):
         df = pd.read_csv('sattogpa.csv')
-        # Ensure the same number of features as the model expects
-        X = df['SAT Score']  # Assuming 'SAT Score' is the only feature
+        X = df['SAT Score'] # only input
         y = df['Grade Point Average']
         regressor = RandomForestRegressor(n_estimators=10, random_state=42)
         regressor.fit(X.values.reshape(-1, 1), y)  # Reshape X to be a 2D array
         # Predicting the GPA
         predicted_gpa = regressor.predict([[self._satscore]])[0]
         return predicted_gpa
-
-# # Flask API
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-# model = SATtoGPAModel()
-
-# @app.route('/satgpacross', methods=['POST'])
-# def predict_gpa():
-#     data = request.get_json()
-#     satscore = data.get('satscore')
-#     if satscore is None:
-#         return jsonify({"error": "Missing SAT score"}), 400
-
-#     # Make prediction using the SATtoGPAModel
-#     gpa_prediction = model.predict(satscore)
-
-#     # Return the prediction as JSON
-#     return jsonify({"GPA_estimate": gpa_prediction})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+    
